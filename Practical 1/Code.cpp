a3a0ccfdb063c43fab98d0eb9c85825a139ba98d
ShresthKumar2002 <113045520+ShresthKumar2002@users.noreply.github.com>
2024-09-09 22:41:45 +0530

    create Practical_1

---
+++ Practical 1/Code.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void cdia(int a);
+
+int main()
+{
+    int a=0;
+    cin>>a;
+    cout<<2*a-1;
+    cdia(a);
+    return 0;
+}
+
+void cdia(int a)
+{
+    int t=2*a-1;
+    char arr[30][30];
+    for(int i=0; i<=t;i++)
+    {
+        for(int j=0; j<t;j++)
+        {
+            arr[i][j] =' ';
+        }
+    }
+    for(int i=0; i<a;i++)
+    {
+        for(int j=a-i;j<a+i;)
+        {
+            arr[i][j]='*';
+            j=j+2;
+        }
+    }
+    for(int j=0;j<t;j=j+2)
+        {
+            arr[a][j]='*';
+        }
+    for(int i=a+1; i<=t;i++)
+    {
+        strcpy(arr[i],arr[t-i+1]);
+    }
+    for(int i=0; i<=t;i++)
+    {
+        for(int j=0; j<t;j++)
+        {
+            cout<<arr[i][j];
+        }
+        cout<<"\n";
+    }
+}

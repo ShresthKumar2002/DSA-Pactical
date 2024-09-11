9eb454c5643aaa6aed1d92bd92acde26a6a83c8c
ShresthKumar2002 <113045520+ShresthKumar2002@users.noreply.github.com>
2024-09-11 23:47:26 +0530

    code modification

--- Practical 1/Code.cpp
+++ Practical 1/Code.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void cdia(int a);
+void cdia(int a);   //Creating a declaration for the function to apply the operation.
 
 int main()
 {
     int a=0;
+    RETRY:cout<<"Enter the number of Stars in the longest row : ";
     cin>>a;
-    cout<<2*a-1;
-    cdia(a);
-    return 0;
+    if(a>=2)
+    {
+        cdia(a);  //Function Call
+        return 0;
+    }
+    else
+    {
+        cout<<"Invalid input\n";
+        goto RETRY;  //Reset
+    }
 }
 
 void cdia(int a)
-{
+{    
     int t=2*a-1;
     char arr[30][30];
+    
+    // A loop to set the 2D array for the Diamond
     for(int i=0; i<=t;i++)
     {
         for(int j=0; j<t;j++)
@@ -23,19 +33,33 @@ void cdia(int a)
             arr[i][j] =' ';
         }
     }
-    for(int i=0; i<a;i++)
+
+    // A Loop to parse from 1st row to n-1 th row
+    for(int i=0; i<=a;i++)
     {
-        for(int j=a-i;j<a+i;)
+        if(i!=a)
         {
-            arr[i][j]='*';
-            j=j+2;
+            // A loop to add stars as per the row number
+            for(int j=a-i;j<a+i;)
+            {
+              arr[i][j]='*';
+              j=j+2;
+            }
+        
+            strcpy(arr[t-i+1],arr[i]);  // Copying the i th row from the top in the i th row from bottom
         }
-        strcpy(arr[t-i+1],arr[i]);
+        else
+        {
+            // A loop to print the center row 
+            for(int j=0;j<t;j=j+2)
+            {
+                arr[a][j]='*';
+            }
+        }
+
     }
-    for(int j=0;j<t;j=j+2)
-        {
-            arr[a][j]='*';
-        }
+
+    // Loop to print the Matrix
     for(int i=0; i<=t;i++)
     {
         for(int j=0; j<t;j++)
